42850065ef85c2bf771a0e2c867296e2224c8df7
maxvonlancaster <windchange7@gmail.com>
2023-12-10 01:00:50 +0200

    upd

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '*',
+        source: '/api/:path*',
         destination: 'https://webtechcnu.netlify.app'
       }
     ]
