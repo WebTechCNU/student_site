04615d396b2ad4ebe16eb932f65eaa206de088b8
maxvonlancaster <windchange7@gmail.com>
2023-12-10 10:58:06 +0200

    upd

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -1,23 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-
-const handler = async (req, res) => {
+export default async function handler(req, res) {
     const client = new MongoClient(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -29,10 +12,11 @@ const handler = async (req, res) => {
     switch (req.method) {
         case 'GET':
             const posts = await db.collection('posts').find({}).toArray();
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-            res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
             res.status(200).json(posts);
+            break;
+        case 'OPTIONS':
+            const postsOpt = await db.collection('posts').find({}).toArray();
+            res.status(200).json(postsOpt);
             break;
         case 'POST':
             const newPost = req.body;
@@ -62,5 +46,3 @@ const handler = async (req, res) => {
 
     await client.close();
 }
-
-module.exports = allowCors(handler)
